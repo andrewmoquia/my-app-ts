9f51fb315a87ff01baef1861cc906df284675943
andrewmoquia <andrewmoquia09@gmail.com>
2021-04-24 16:58:56 +0800

    Update App component.

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import "./App.scss";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Rick and Morty!</h1>
+      <p>Pick your favorite episode!</p>
+    </React.Fragment>
+  );
+}
+
+export default App;
