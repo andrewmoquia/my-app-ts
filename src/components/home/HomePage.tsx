22e12c6dc8edf51af3981433ea21df68e0259a9a
andrewmoquia <80556699+andrewmoquia@users.noreply.github.com>
2021-05-02 22:41:02 +0800

    Update HomePage.tsx

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
   });
 
   const props: IEpisodeProps = {
-    episodes: state.episodes,
+    episodes: state.episodes, 
     favorites: state.episodes,
     store: { state, dispatch },
     toogleFavoriteAction
